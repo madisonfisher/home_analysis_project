ae0cd3c8a95a4a57056cb279e554e6df2c4f8d09
madisonfisher <mbfishe2@ncsu.edu>
2021-08-13 13:26:56 -0400

    moved css into html and updated formatting of graph

--- static/js/inventory_map.js
+++ static/js/inventory_map.js
@@ -1,13 +1,8 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
 var myMap = L.map("map", {
   center: [39.8283, -98.5795],
   zoom: 5
 });
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -94,7 +89,7 @@ d3.json("../home_inventory_analysis/latlong_cities.json").then((data5) => {
               color: 'rgb(0, 0, 139)'
             },
             name: "Mean",
-            legendgroup: 'group1'
+            legendgroup: 'group1',
           };
 
           d3.json("../home_inventory_analysis/base_inventory_files/city_median_pending_weekly_smooth_base_modified.json").then((data4) => {
@@ -132,14 +127,15 @@ d3.json("../home_inventory_analysis/latlong_cities.json").then((data5) => {
             width: 600,
             legend: {
               "orientation": "h",
-              x: 0,
-              y: -1
+              x: 0.25,
+              y: -0.5,
             },
             margin: {
               t: 60, 
               l: 40, 
               r: 20,
-              b: 20 }
+              b: 20 },
+            hovermode: "closest"
           };
 
           var content = `<div id=graph${j}></div>`;
