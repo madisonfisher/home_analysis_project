dc9f80448b198296b9bf33abbf96eb7b1ee9ec3d
Henry Poe <hpoe111@gmail.com>
2021-08-14 17:14:57 -0400

    more value analysis page formatting

--- static/js/home_value_trends.js
+++ static/js/home_value_trends.js
@@ -1,9 +1,9 @@
 /**
  * Graph trends of home value across the U.S
- * Last edited on 8/9/2021
+ * Last edited on 8/14/2021
  */
 
-function createTrace (homeValueRecord, yearKeys) {
+ function createTrace (homeValueRecord, yearKeys) {
     var homeValues = [];
     for(key in yearKeys) {
         homeValues.push(homeValueRecord[yearKeys[key]]);
@@ -38,6 +38,7 @@ d3.csv("../static/data/State_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv").t
     var layout = {
         title:'Home values for all States and DC since 1996',
         hovermode: 'closest',
+        height: 525,
         xaxis: {
             title: {
                 text: 'Year',
@@ -74,6 +75,7 @@ d3.csv("../static/data/9604_State_Home_Values_LINEARpredicted.csv").then(functio
     var layout = {
         title:'1996 - 2003 Test',
         hovermode: 'closest',
+        height: 525,
         xaxis: {
             title: {
                 text: 'Year',
@@ -110,6 +112,7 @@ d3.csv("../static/data/1019_State_Home_Values_LINEARpredicted.csv").then(functio
     var layout = {
         title:'2010-2019 Test',
         hovermode: 'closest',
+        height: 525,
         xaxis: {
             title: {
                 text: 'Year',
@@ -130,6 +133,7 @@ d3.csv("../static/data/All_June_2021_Values.csv").then(function(data) {
     var columns = ['Date','Actual','96-03 Growth','10-19 Growth']
 
     var table = d3.select('#table').append('table')
+    table.classed('table table-bordered table-secondary table-striped table-sm', true)
     var thead = table.append('thead')
     var tbody = table.append('tbody')
 
@@ -142,9 +146,21 @@ d3.csv("../static/data/All_June_2021_Values.csv").then(function(data) {
 
     for (i = 0; i < data.length; i++) {
         var row = tbody.append('tr');
-        console.log(data[i].length);
-        for (j = 0; j < data[i].length; j++){
-            console.log(j);
+
+        for (key in data[i]) {
+            if (key == 'Actual')
+                row.append('td')
+                    .text(parseInt(data[i][key]).toLocaleString('en-US'));
+            else if (key == '96-03 Growth' || key == '10-19 Growth')
+                if (isNaN(parseFloat(data[i][key])))
+                    row.append('td')
+                        .text("");
+                else
+                    row.append('td')
+                        .text(parseFloat(parseFloat(data[i][key]).toFixed(2)).toLocaleString('en-US'));
+            else
+                row.append('td')
+                    .text(data[i][key]);
         }
     }
 });
