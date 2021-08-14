e2b22ac3346c7f7e0f0d19716008d16dabb80b33
madisonfisher <mbfishe2@ncsu.edu>
2021-08-14 10:29:26 -0400

    adding github secrects information

--- docs/static/js/inventory_map.js
+++ docs/static/js/inventory_map.js
@@ -9,7 +9,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
-  accessToken: secrets.API_KEY
+  accessToken: API_KEY
 }).addTo(myMap);
 
 d3.json("Data/latlong_cities.json").then((data5) => {
